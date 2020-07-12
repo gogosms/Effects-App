fcfc0c2475bd0b9700ced36233fd86cc7dbf6457
rodrigo valdez <rodrigo.valdez@bechtle.com>
2020-07-12 20:16:54 -0300

    initial App

---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario.model';
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html'
+
+})
+export class ListaComponent implements OnInit {
+
+  usuarios: Usuario[] = [];
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+    this.usuarioService.getUsers()
+      .subscribe(usuarios => this.usuarios = usuarios);
+  }
+
+}
